d3be112279cc63bbcc9bbc70ff062a60c10b8628
Stephen Leander <stephen.leander1@gmail.com>
2022-05-19 11:25:41 +0200

    Move error handling to catch in getWeather function

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-function handleError(fn) {
-  return function (...params) {
-    return fn(...params).catch(function (err) {
-      console.log(err);
-    });
-  };
-}
-
 async function getWeather(location, units) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=4876ad9cbecdbf1c86ba8fafe3c93d9a`,
-    { mode: 'cors' }
-  );
-  const weatherData = await response.json();
-  console.log(weatherData);
-  return weatherData;
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=4876ad9cbecdbf1c86ba8fafe3c93d9a`,
+      { mode: 'cors' }
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+    return weatherData;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function formattedData(location) {
