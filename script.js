0865e352f87ecca06f3d82fdfa7d095e6f098a7b
Stephen Leander <stephen.leander1@gmail.com>
2022-05-20 11:19:17 +0200

    Add button to change units

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ async function getWeather(location, units) {
   }
 }
 
-async function formatWeather(location) {
+async function formatWeather(weather) {
   try {
-    const data = await location;
+    const data = await weather;
     const info = {};
     info.name = data.name;
     info.temp = data.main.temp;
@@ -24,33 +24,63 @@ async function formatWeather(location) {
   }
 }
 
+async function displayWeather(data, units, element) {
+  try {
+    const weather = await formatWeather(getWeather(data, units));
+    if (weather) {
+      element.textContent = '';
+    }
+    for (let i = 0; i < Object.keys(weather).length; i++) {
+      const keyDiv = document.createElement('div');
+      keyDiv.textContent = Object.values(weather)[i];
+      element.appendChild(keyDiv);
+    }
+  } catch (error) {
+    console.log(error);
+    alert('Location not found.');
+  }
+}
+
 function addSearchbar(element) {
   const search = document.createElement('input');
+  search.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      document.querySelector('.search').click();
+    }
+  });
   element.appendChild(search);
   const searchSubmit = document.createElement('button');
   searchSubmit.textContent = 'Search';
+  searchSubmit.classList.toggle('search');
   searchSubmit.addEventListener('click', () => {
-    displayWeather(search.value, document.body);
+    displayWeather(search.value, units, weatherInfo);
+    search.value = '';
   });
   element.appendChild(searchSubmit);
 }
 
-async function displayWeather(data, element) {
-  try {
-    const weather = await formatWeather(getWeather(data, 'imperial'));
-    console.log(weather);
-    for (let i = 0; i < Object.keys(weather).length; i++) {
-      const body = document.querySelector('body');
-      const keyDiv = document.createElement('div');
-      keyDiv.textContent = Object.values(weather)[i];
-      body.appendChild(keyDiv);
-    }
-  } catch (error) {
-    console.log(error);
-    alert('City not found.');
-  }
-}
-
 const body = document.querySelector('body');
+const weatherInfo = document.createElement('div');
+weatherInfo.classList.toggle('weatherInfo');
+let units = 'imperial';
 
+const unitChanger = document.createElement('button');
+unitChanger.type = 'button';
+unitChanger.textContent = '°F';
+unitChanger.addEventListener('click', () => {
+  const currentLocation = weatherInfo.querySelector('div').textContent;
+  if (units === 'imperial') {
+    units = 'metric';
+    unitChanger.textContent = '°C';
+    displayWeather(currentLocation, units, weatherInfo);
+  } else {
+    units = 'imperial';
+    unitChanger.textContent = '°F';
+    displayWeather(currentLocation, units, weatherInfo);
+  }
+});
+body.appendChild(unitChanger);
+
+body.appendChild(weatherInfo);
+displayWeather('Madrid', units, weatherInfo);
 addSearchbar(body);
