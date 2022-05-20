eb2ea807c263f6ac28fb1173a3675e89d2a5039a
Stephen Leander <stephen.leander1@gmail.com>
2022-05-20 11:40:08 +0200

    Refactor and organize functions

--- script.js
+++ script.js
@@ -16,7 +16,11 @@ async function formatWeather(weather) {
     const data = await weather;
     const info = {};
     info.name = data.name;
-    info.temp = data.main.temp;
+    if (units === 'imperial') {
+      info.temp = `${data.main.temp} °F`;
+    } else {
+      info.temp = `${data.main.temp} °C`;
+    }
     info.description = data.weather[0].description;
     return info;
   } catch (error) {
@@ -32,6 +36,7 @@ async function displayWeather(data, units, element) {
     }
     for (let i = 0; i < Object.keys(weather).length; i++) {
       const keyDiv = document.createElement('div');
+      keyDiv.classList.toggle(`${Object.keys(weather)[i]}`);
       keyDiv.textContent = Object.values(weather)[i];
       element.appendChild(keyDiv);
     }
@@ -40,47 +45,55 @@ async function displayWeather(data, units, element) {
     alert('Location not found.');
   }
 }
-
-function addSearchbar(element) {
-  const search = document.createElement('input');
-  search.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-      document.querySelector('.search').click();
-    }
-  });
-  element.appendChild(search);
-  const searchSubmit = document.createElement('button');
-  searchSubmit.textContent = 'Search';
-  searchSubmit.classList.toggle('search');
-  searchSubmit.addEventListener('click', () => {
-    displayWeather(search.value, units, weatherInfo);
-    search.value = '';
-  });
-  element.appendChild(searchSubmit);
-}
-
+///////////////////////////////////////////////
 const body = document.querySelector('body');
-const weatherInfo = document.createElement('div');
-weatherInfo.classList.toggle('weatherInfo');
+const weatherContainer = document.createElement('div');
+weatherContainer.classList.toggle('weatherContainer');
+body.appendChild(weatherContainer);
 let units = 'imperial';
 
-const unitChanger = document.createElement('button');
-unitChanger.type = 'button';
-unitChanger.textContent = '°F';
-unitChanger.addEventListener('click', () => {
-  const currentLocation = weatherInfo.querySelector('div').textContent;
-  if (units === 'imperial') {
-    units = 'metric';
-    unitChanger.textContent = '°C';
-    displayWeather(currentLocation, units, weatherInfo);
-  } else {
-    units = 'imperial';
-    unitChanger.textContent = '°F';
-    displayWeather(currentLocation, units, weatherInfo);
-  }
-});
-body.appendChild(unitChanger);
+function addSearchbar(element) {
+  const searchBar = document.createElement('div');
+  searchBar.classList.toggle('searchBar');
+  const searchInput = document.createElement('input');
+  searchInput.classList.toggle('searchInput');
+  searchInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      document.querySelector('.searchButton').click();
+    }
+  });
+  searchBar.appendChild(searchInput);
+  const searchSubmit = document.createElement('button');
+  searchSubmit.textContent = 'Search';
+  searchSubmit.classList.toggle('searchButton');
+  searchSubmit.addEventListener('click', () => {
+    displayWeather(searchInput.value, units, weatherContainer);
+    searchInput.value = '';
+  });
+  searchBar.appendChild(searchSubmit);
+  element.appendChild(searchBar);
+}
 
-body.appendChild(weatherInfo);
-displayWeather('Madrid', units, weatherInfo);
+function addUnitButton(element) {
+  const unitButton = document.createElement('button');
+  unitButton.classList.toggle('unitButton');
+  unitButton.type = 'button';
+  unitButton.textContent = '°F';
+  unitButton.addEventListener('click', () => {
+    const currentLocation = weatherContainer.querySelector('div').textContent;
+    if (units === 'imperial') {
+      units = 'metric';
+      unitButton.textContent = '°C';
+      displayWeather(currentLocation, units, weatherContainer);
+    } else {
+      units = 'imperial';
+      unitButton.textContent = '°F';
+      displayWeather(currentLocation, units, weatherContainer);
+    }
+  });
+  element.appendChild(unitButton);
+}
+
+displayWeather('Madrid', units, weatherContainer);
 addSearchbar(body);
+addUnitButton(body);
